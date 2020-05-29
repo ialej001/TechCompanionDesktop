e0356df96e5c590b05e7d755b004df2f3c11da76
ialej001 <ivan.alejandre@gmail.com>
2020-05-29 16:08:45 -0700

    fixed new service call would not create a new customer

--- desktop/src/renderer/models/ServiceCall.js
+++ desktop/src/renderer/models/ServiceCall.js
@@ -1,7 +1,9 @@
+import Customer from "@/models/Customer.js";
+
 export default class ServiceCall {
   constructor() {
     this.string_id = null;
-    this.customer = {};
+    this.customer = new Customer();
     this.techAssigned = null;
     this.issues = [];
   }
